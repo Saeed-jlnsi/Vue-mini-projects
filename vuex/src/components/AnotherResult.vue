7da957e3097d7608c8ee2ac08ca3f585385380a3
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2019-04-07 20:30:40 +0430

    Use GETTERS in VUEX

--- vuex/src/components/AnotherResult.vue
+++ vuex/src/components/AnotherResult.vue
@@ -6,6 +6,8 @@
     export default {
         computed: {
             counter() {
+                // WE DON'T EXECUTE THE FUNCTION => doubleCounter();
+                // THE VUEX WILL EXECUTE IT FOR US(so don't need '()' in front of the function).
                 return this.$store.getters.doubleCounter;
             } 
         }
