81a89bac671e6367d4a366e9c4fee24a20ae9b2d
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2019-04-16 01:37:05 +0430

    Improving STORE folder structure(using MODULES)

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -1,31 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Counter from './modules/counter';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value: 0
     },
     getters: {
-        doubleCounter: state => {
-            return state.counter * 2;
-        },
-        stringCounter: state => {
-            return state.counter + " clicks";
-        },
         value: state => {
             return state.value;
         }
     },
     mutations: {
-        increment: (state, payload) => {
-            state.counter += payload;
-        },
-        decrement: (state, payload) => {
-            state.counter -= payload;
-        },
         updateValue: (state, payload) => {
             state.value = payload;
         }
@@ -38,25 +26,11 @@ export const store = new Vuex.Store({
 
     // WE USE DESTRUCTION TO USE ONLY THE COMMIT METHOD OF THE CONTEXT OBJECT: '{commit}'
     actions: {
-        increment: ({commit}, payload) => {
-            commit('increment', payload);
-        },
-        decrement: ({commit}, payload) => {
-            commit('decrement', payload);
-        },
-        // HERE THE PAYLOAD PARAMETER IS AN OBJECT, AND WE USED IT AS AN OBJECT('payload.by' and 'payload.duration')
-        asyncIncrement: ({commit}, payload) => {
-            setTimeout(() => {
-                commit('increment', payload.by);
-            }, payload.duration);
-        },
-        asyncDecrement: ({commit}, payload) => {
-            setTimeout(() => {
-                commit('decrement', payload.by);
-            },  payload.duration);
-        },
         updateValue: ({commit}, payload) => {
             commit('updateValue', payload);
         }
+    },
+    modules: {
+        Counter
     }
 });
