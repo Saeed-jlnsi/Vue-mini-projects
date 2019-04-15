c267b44253bee5a859d9e2f60f1f371942ba95c0
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2019-04-15 22:07:15 +0430

    Use MAPGETTER METHOD && install the dependency needed to use the SPREAD OPERATOR

--- vuex/src/components/AnotherResult.vue
+++ vuex/src/components/AnotherResult.vue
@@ -1,15 +1,40 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Counter is: {{ stringCounter }}</p>
+    </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
     export default {
+        // computed: {
+        //     counter() {
+        //         // WE DON'T EXECUTE THE FUNCTION => doubleCounter();
+        //         // THE VUEX WILL EXECUTE IT FOR US(so don't need '()' in front of the function).
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+
+        // INSTEAD OF DOING THE ABOVE FOR EACH GETTER, WE CAN USE THE 'mapGetters' TO USE ALL THE GETTERS WE NEED IN THE COMPONENT 
+        // computed: mapGetters([
+        //         'doublerCounter',
+        //         'stringCounter'
+        //     ])
+
         computed: {
-            counter() {
-                // WE DON'T EXECUTE THE FUNCTION => doubleCounter();
-                // THE VUEX WILL EXECUTE IT FOR US(so don't need '()' in front of the function).
-                return this.$store.getters.doubleCounter;
-            } 
+            // WE USE THE SPREAD OPERATOR TO BE ABLE TO DEFINE OUR OWN COMPUTED PROPERTIES ALONGSIDE THE MAPGETTERS METHOD
+            // TO USE THE SREAD OPERATOR WE SHOULD INSTALL A DEPENDENCY 'npm install --save-dev babel-preset-stage-2'
+            ...mapGetters([
+                'doubleCounter',
+                'stringCounter'
+            ]),
+            // ourOwn() {
+            //    //NOW WE CAN DEFINE OUR OWN COMPUTED PROPERTY
+            // }
         }
     }
 </script>
