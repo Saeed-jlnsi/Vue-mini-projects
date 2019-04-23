c62b57ab8177a0dd7f55eea4270b54fd5aeb7158
saeed-jlnsi <saeed.jalinoosi@gmail.com>
2019-04-23 20:25:37 +0430

    Create the HEADER and the main structure of the app

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="container">
-        
+        <header class="well">
+            <nav>
+                <ul>
+                    <li>Stock Trader</li>
+                    <li>Portfolio</li>
+                    <li>Stocks</li>
+                </ul>
+            </nav>
+            <div class="utiles">
+                <ul>
+                    <li>End Day</li>
+                    <li>Save & Load</li>
+                    <li>Funds: $0</li>
+                </ul>
+            </div>
+        </header>
+        <div class="content">
+
+        </div>
     </div>
 </template>
 
@@ -12,5 +30,44 @@
 </script>
 
 <style>
+html,
+body {
+    height: 100%;
+    width: 100%;
+}
+body {
+    font-size: 14px;
+    color: #444;
+}
+ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    margin: 0;
+    padding: 0;
+}
+li {
+    list-style: none;
+    cursor: pointer;
+}
+header {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 40px;
+}
+nav {
+    width: 30%;
+    display: flex;
+    align-items: center;
+}
+nav ul {
+    width: 100%;
+}
+nav ul li:first-child {
+    font-size: 1.2em;
+}
+.utiles {
+    width: 30%;
+}
 
 </style>
